36d7f843243121e9589bc31b06f694ab5acef2c5
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-06-04 12:24:52 +0300

    Add SimpleServer

---
+++ 01.AsynchronousProcessing/03.SimpleServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _03.SimpleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter local port number:");
+            int port = int.Parse(Console.ReadLine());
+
+            var server = new SimpleServer(port);
+            Task task = server.Run();
+
+            string input;
+            if ((input = Console.ReadLine()) == "exit")
+            {
+                server.Stop();
+            }
+        }
+    }
+}
+ 
---
+++ 01.AsynchronousProcessing/03.SimpleServer/SimpleServer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03.SimpleServer
+{
+    public class SimpleServer
+    {
+        private readonly TcpListener listener;
+
+        public SimpleServer(int port)
+        {
+            listener = new TcpListener(IPAddress.Loopback, port);
+        }
+
+        public async Task Run()
+        {
+            listener.Start();
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for client...");
+                var client = await listener.AcceptTcpClientAsync();
+                HandleClient(client);
+            }
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            var stream = client.GetStream();
+
+            while (!stream.DataAvailable)
+            {
+                Thread.Sleep(1);
+            }
+            string request = string.Empty;
+            while (stream.DataAvailable)
+            {
+                byte[] buffer = new byte[1024];
+                stream.Read(buffer, 0, buffer.Length);
+                request += Encoding.UTF8.GetString(buffer);
+            }
+            Console.WriteLine(request);
+
+            var responseString = File.ReadAllText(@"Http\HttpResponse.txt");
+            var responseBytes = Encoding.UTF8.GetBytes(responseString);
+            stream.Write(responseBytes);
+        }
+
+        public void Stop()
+        {
+            listener.Stop();
+        }
+    }
+}
